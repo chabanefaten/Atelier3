09caff89721f8ff9b2135fa317a7c9c963025fd6
chabanefaten <fatenchabane@gmail.com>
2023-10-31 18:31:52 +0100

    first commit

--- src/app/add-produit/add-produit.component.ts
+++ src/app/add-produit/add-produit.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Produit } from '../model/produit.model';
 import { ProduitService } from '../services/produit.service';
+import { Categorie } from '../model/categorie.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-produit',
@@ -9,18 +11,28 @@ import { ProduitService } from '../services/produit.service';
 export class AddProduitComponent implements OnInit {
 
   newProduit = new Produit();
+  categories! : Categorie[];
+  newIdCat! : number;
+  newCategorie! : Categorie;
 
   message :string | undefined;
 
-  constructor(private produitService: ProduitService) { }
+  constructor(private produitService: ProduitService,
+                       private router :Router) {}
 
   ngOnInit(): void {
+    this.categories = this.produitService.listeCategories();
+
   }
 
   addProduit() {
-    //console.log(this.newProduit);
+    console.log(this.newIdCat);
+    this.newCategorie =
+    this.produitService.consulterCategorie(this.newIdCat);
+    this.newProduit.categorie = this.newCategorie;
+    this.router.navigate(['produits'])
     this.produitService.ajouterProduit(this.newProduit);
-    this.message ="Produit " + this.newProduit.nomProduit + "ajoute avec succes !" ;
+    this.message =" Produit " + this.newProduit.nomProduit + "ajoute avec succes !" ;
   }
 
 }
